b0e4d4b5b01f78800a4f44319b3bf7c35153d00d
Deepak Jagadeesh <djagadeesh@paypal.com>
2019-10-04 00:44:02 -0700

    feat(LaunchItem): render graphql launches response with LaunchItem component

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,10 +1,11 @@
 // class based component
 // use ES& react/redux/graphql-native snippets externsion in vscode to generate class based component easily
 // after installing the plug in hit rce tab, boom u get the default snippet
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 // used to make query
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem';
 
 // create a query similar to how we do it in graphiql
 // gql with back ticks is the way you define a query from the client side using gql library
@@ -21,10 +22,13 @@ const LAUNCHES_QUERY = gql`
   }
 `;
 
+// Launches returns a dummy Fragment element with GraphQL query response
+// destructing of GraphQL Query response will give loading, error, and data
+// For each item to render, we wrap it with LaunchItem component
 export class Launches extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <h1 className="display-4 my-3">Launches</h1>
           <Query query={LAUNCHES_QUERY}>
             {
@@ -32,11 +36,17 @@ export class Launches extends Component {
                 if (loading) return <h4>Loading...</h4>
                 if(error) console.log(error);
                 console.log(data);
-                return <h1>data</h1>
+                return <Fragment>
+                  {
+                    data.launches.map(launch => (
+                      <LaunchItem key={launch.flight_number} launch={launch} />
+                    ))
+                  }
+                </Fragment>
               }
             }
           </Query>
-      </div>
+      </Fragment>
     )
   }
 }
