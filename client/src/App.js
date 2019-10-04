bd878b02a0db8f1462d29dcfa813dfeca0e58c27
Deepak Jagadeesh <djagadeesh@paypal.com>
2019-10-03 23:49:04 -0700

    feat(apollo client): add apollo client and wrap it with provider for state management

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,24 @@ import React from 'react';
 import './App.css';
 import logo from './logo.png';
 
+// get ApolloClient everything to setup apollo client
+import ApolloClient from 'apollo-boost';
+// this is similar to redux for state management. simply wrap the apollo client inside the
+// ApolloProvider and pass it on.
+import { ApolloProvider } from 'react-apollo';
+
+// create an ApolloClient that hits the graphql server end point from front end
+const client = new ApolloClient( {
+  uri: 'http://localhost:5000/graphql'
+})
+
 function App() {
   return (
-    <div className="App">
-      <img src={logo} alt="SpaceX" style={{ width: 800, height: 200, display: 'block', margin: 'auto' }}></img>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="component">
+        <img src={logo} alt="SpaceX" style={{ width: 800, height: 150, display: 'block', margin: 'auto' }}></img>
+      </div>
+    </ApolloProvider>
   );
 }
 
