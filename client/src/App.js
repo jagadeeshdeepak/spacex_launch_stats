271201b52df0ebbefa9ed1370b35d3343701a241
Deepak Jagadeesh <djagadeesh@paypal.com>
2019-10-03 23:16:41 -0700

    feat(logo): add new logo for spacex

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
-      <h1>SpaceX</h1>
+      <img src={logo} alt="SpaceX" style={{ width: 800, height: 200, display: 'block', margin: 'auto' }}></img>
     </div>
   );
 }
