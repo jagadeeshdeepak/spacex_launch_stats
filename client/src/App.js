884a7d5c748c0417a421e3bfe813b3988d139d9c
Deepak Jagadeesh <djagadeesh@paypal.com>
2019-10-04 01:21:27 -0700

    feat(Router/Route): add a Router and Route for browser navigation for the client app

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,25 @@ import ApolloClient from 'apollo-boost';
 // this is similar to redux for state management. simply wrap the apollo client inside the
 // ApolloProvider and pass it on.
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Launches from './components/Launches';
+
 
 // create an ApolloClient that hits the graphql server end point from front end
 const client = new ApolloClient( {
   uri: 'http://localhost:5000/graphql'
 })
 
+// add Router and tell whats the component that needs to be rendered
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="component" style={{ marginLeft: '75px', marginRight: '75px' }}>
-        <img src={logo} alt="SpaceX" style={{ width: 500, height: 100, display: 'block', margin: 'auto' }}></img>
-        <Launches></Launches>
-      </div>
+      <Router>
+        <div className="container">
+          <img src={logo} alt="SpaceX" style={{ width: 500, height: 100, display: 'block', margin: 'auto' }}></img>
+          <Route exact path="/" component={Launches}/>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
